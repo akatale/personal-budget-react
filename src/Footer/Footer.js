54a8a14838e6a005abcd957438326d01050293db
Ashwin Katale <akatale@uncc.edu>
2020-10-11 17:31:02 -0400

    brought pbudget code

---
+++ src/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div className="bottom">
+    <div className="center">
+        All rights reserved &copy; Ashwin Katale
+    </div>
+</div>
+  );
+}
+
+export default Footer;
